5e4bed94b1a1309d9d5f5e75140f13ba004cc736
imxcstar <1454643841@qq.com>
2022-07-08 01:02:27 +0800

    修复一些回调方法的乱码

--- Build/卸载.bat
+++ Build/卸载.bat
@@ -1,11 +1,11 @@
 @echo off
 if %PROCESSOR_ARCHITECTURE% == AMD64 (
-if not exist "%windir%\Syswow64\MiniblinkSDK_201.dll" goto ends
-regsvr32 "%windir%\Syswow64\MiniblinkSDK_201.dll" /s /u
-del /f /s /q "%windir%\Syswow64\MiniblinkSDK_201.dll"
+if not exist "%windir%\Syswow64\MiniblinkSDK_202.dll" goto ends
+regsvr32 "%windir%\Syswow64\MiniblinkSDK_202.dll" /s /u
+del /f /s /q "%windir%\Syswow64\MiniblinkSDK_202.dll"
 ) else (
-if not exist "%windir%\System32\MiniblinkSDK_201.dll" goto ends
-regsvr32 "%windir%\System32\MiniblinkSDK_201.dll" /s /u
-del /f /s /q "%windir%\System32\MiniblinkSDK_201.dll"
+if not exist "%windir%\System32\MiniblinkSDK_202.dll" goto ends
+regsvr32 "%windir%\System32\MiniblinkSDK_202.dll" /s /u
+del /f /s /q "%windir%\System32\MiniblinkSDK_202.dll"
 )
 :ends
--- Build/注册.bat
+++ Build/注册.bat
@@ -1,11 +1,11 @@
 @echo off
 @set root=%~dp0
 if %PROCESSOR_ARCHITECTURE% == AMD64 (
-if exist "%windir%\Syswow64\MiniblinkSDK_201.dll" goto ends
-xcopy "%root%MiniblinkSDK_201.dll" "%windir%\Syswow64\" /s/y
-regsvr32 "%windir%\Syswow64\MiniblinkSDK_201.dll" /s
+if exist "%windir%\Syswow64\MiniblinkSDK_202.dll" goto ends
+xcopy "%root%MiniblinkSDK_202.dll" "%windir%\Syswow64\" /s/y
+regsvr32 "%windir%\Syswow64\MiniblinkSDK_202.dll" /s
 ) else (
-if exist "%windir%\System32\MiniblinkSDK_201.dll" goto ends
-xcopy "%root%MiniblinkSDK_201.dll" "%windir%\System32\" /s/y
-regsvr32 "%windir%\System32\MiniblinkSDK_201.dll" /s)
+if exist "%windir%\System32\MiniblinkSDK_202.dll" goto ends
+xcopy "%root%MiniblinkSDK_202.dll" "%windir%\System32\" /s/y
+regsvr32 "%windir%\System32\MiniblinkSDK_202.dll" /s)
 :ends
